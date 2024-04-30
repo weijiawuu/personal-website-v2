30586f31a4d334bb6bfaf7b73cad8f4f452eb2f6
Weijia Wu <weijiawu@umich.edu>
2024-04-29 23:36:12 -0400

    fixed links and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className="border-b-2" style={{ borderColor: '#8e8d8a' }}>
+    <div className="border-b-4" style={{ borderColor: '#8e8d8a', backgroundColor: 'white' }}>
       <div className="flex justify-between items-center h-[80px] max-w-[1600px] mx-auto font-bold text-[#8e8d8a] text-xl">
-        <Link to="https://www.linkedin.com/in/weijiawu2004/" className='text-3xl font-bold text-[#8e8d8a] hover:text-[#e85a4f]'>
+        <Link to="https://www.linkedin.com/in/weijiawuwork/" className='text-3xl font-bold text-[#8e8d8a] hover:text-[#e85a4f]'>
           Weijia
         </Link>
         <ul className='hidden md:flex'>
@@ -33,22 +33,6 @@ const Navbar = () => {
         <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
-
-        <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[white] ease-in-out duration-500' : 'dsease-in-out duration-500 fixed left-[-100%]'}>
-          <h1 className='w-full text-3xl font-bold p-4 border-b border-gray-600'>Pages</h1>
-          <li className='p-4 border-b border-gray-600'>
-            <Link to="/" className='hover:text-[#e85a4f]'>Home</Link>
-          </li>
-          <li className='p-4 border-b border-gray-600'>
-            <Link to="/about" className='hover:text-[#e85a4f]'>About</Link>
-          </li>
-          <li className='p-4 border-b border-gray-600'>
-            <Link to="/contact" className='hover:text-[#e85a4f]'>Newsletter</Link>
-          </li>
-          <li className='p-4 border-b border-gray-600'>
-            <Link to="/resume" className='hover:text-[#e85a4f]'>Contact</Link>
-          </li>
-        </ul>
       </div>
     </div>
   );
